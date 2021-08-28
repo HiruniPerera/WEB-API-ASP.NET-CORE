8dfe089e7e4ed331ddf648737b18d2f40e5d630f
Hiruni Perera <hirunidivyangani@gmail.com>
2021-08-28 23:11:24 +0530

    files added.

---
+++ bookapi/bookapi/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bookapi.Models
+{
+	public class Book
+	{
+		public int Id { get; set; }
+		public String Title { get; set; }
+		public String Author { get; set; }
+		public String Description { get; set; }
+	}
+}
---
+++ bookapi/bookapi/Models/Bookcontext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bookapi.Models
+{
+	public class Bookcontext :DbContext 
+	{
+		public Bookcontext(DbContextOptions<Bookcontext>options)
+			:base(options)
+		{
+
+		}
+		public DbSet<Book> Books { get; set; }
+	}
+}
